52dfa1b1c5f4d4ca653c5c1ebe2a62614bbb4c26
Omar Saavedra <nftsaavedra@gmail.com>
2026-03-02 11:32:09 -0500

    feat: añade bloque de "entradas recientes" con estilos y lo integra en las plantillas principales del tema.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template de fallback principal del tema hijo.
  *
@@ -11,18 +12,20 @@
 get_header();
 ?>
 
-<main id="main-content" class="site-main">
-    <?php
-    if ( have_posts() ) :
-        while ( have_posts() ) :
-            the_post();
-            get_template_part( 'template-parts/content', get_post_type() );
-        endwhile;
-        the_posts_navigation();
-    else :
-        get_template_part( 'template-parts/content', 'none' );
-    endif;
-    ?>
+<main id="main-content" class="site-main dt-py-default">
+    <div class="dt-container">
+        <?php
+        if (have_posts()) :
+            while (have_posts()) :
+                the_post();
+                get_template_part('template-parts/content', get_post_type());
+            endwhile;
+            the_posts_navigation();
+        else :
+            get_template_part('template-parts/content', 'none');
+        endif;
+        ?>
+    </div>
 </main>
 
 <?php
--- page.php
+++ page.php
@@ -1,49 +1,52 @@
 <?php
+
 /**
  * The template for displaying all pages.
  *
  * @package ViceUnf
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined('ABSPATH')) {
     exit;
 }
 
 get_header();
 ?>
 
-<div class="dt-container">
-    <div class="dt-row dt-g-5">
-        <?php 
-        $has_sidebar = is_active_sidebar( 'viceunf-sidebar-primary' );
-        $main_class  = $has_sidebar ? 'dt-col-lg-8' : 'dt-col-lg-12';
-        ?>
-        <div id="dt-main" class="<?php echo esc_attr( $main_class ); ?>">
-            <?php while ( have_posts() ) : the_post(); ?>
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <div class="dt_page_content entry-content">
-                        <?php
-                        the_content();
-                        wp_link_pages( array(
-                            'before' => '<div class="page-links">' . esc_html__( 'Páginas:', 'viceunf' ),
-                            'after'  => '</div>',
-                        ) );
-                        ?>
-                    </div>
-                </article>
+<div id="content" class="site-content dt-py-default">
+    <div class="dt-container">
+        <div class="dt-row dt-g-5">
+            <?php
+            $has_sidebar = is_active_sidebar('viceunf-sidebar-primary');
+            $main_class  = $has_sidebar ? 'dt-col-lg-8' : 'dt-col-lg-12';
+            ?>
+            <div id="dt-main" class="<?php echo esc_attr($main_class); ?>">
+                <?php while (have_posts()) : the_post(); ?>
+                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                        <div class="dt_page_content entry-content">
+                            <?php
+                            the_content();
+                            wp_link_pages(array(
+                                'before' => '<div class="page-links">' . esc_html__('Páginas:', 'viceunf'),
+                                'after'  => '</div>',
+                            ));
+                            ?>
+                        </div>
+                    </article>
 
-                <?php
-                if ( comments_open() || get_comments_number() ) :
-                    comments_template();
-                endif;
-                ?>
-            <?php endwhile; ?>
+                    <?php
+                    if (comments_open() || get_comments_number()) :
+                        comments_template();
+                    endif;
+                    ?>
+                <?php endwhile; ?>
+            </div>
+            <?php
+            if ($has_sidebar) {
+                get_sidebar();
+            }
+            ?>
         </div>
-        <?php 
-        if ( $has_sidebar ) {
-            get_sidebar();
-        }
-        ?>
     </div>
 </div>
 
